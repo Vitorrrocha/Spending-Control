d9ddf34f3f4e507410335e2aa14fdcaa0d7ef26a
vitorrrocha <vitor@gntech.med.br>
2021-03-16 10:36:26 -0300

    Feat: template and models add Transacao

--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from .models import Transacao
 import datetime
 
 def home(request):
@@ -8,3 +7,8 @@ def home(request):
     data['transacoes'] = ['t1','t2','t3']
     data['now']= datetime.datetime.now()
     return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
